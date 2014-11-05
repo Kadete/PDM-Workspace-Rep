4f14d38aa8a3efca172fde396ae9520ca8d87ffc
Tiago Martins <tiagofcmartins@gmail.com>
2014-11-05 18:28:39 +0000

    Deixa de utilizar Strings constantes para utilizar String resources

--- src/ThothNews/app/src/main/java/isel/pdm/serie1/thothNews/AlertDialogFragment.java
+++ src/ThothNews/app/src/main/java/isel/pdm/serie1/thothNews/AlertDialogFragment.java
@@ -4,15 +4,10 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.content.SharedPreferences;
-import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.text.Html;
 import android.widget.Toast;
-
-import java.util.Set;
 
 /**
  * Created by Kadete on 02/11/2014.
@@ -21,23 +16,30 @@ public class AlertDialogFragment extends DialogFragment {
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        String clean = getString((R.string.clean_classes_title));
+        String warning = getString(R.string.clean_classes_warning);
+        String ok = getString(R.string.clean_clasess_ok);
+        String cancel = getString(R.string.clean_classes_cancel);
+        final String toastMessage = getString(R.string.clean_classes_toast);
+
         return new AlertDialog.Builder(getActivity())
                 // Set Dialog Icon
                 .setIcon(R.drawable.thoth_icon)
                 // Set Dialog Title
-                .setTitle("Clean Your Preferences")
+                .setTitle(clean)
                 // Set Dialog Message
-                .setMessage("Carefull!! You will lose all the previous preferences stored!")
+                .setMessage(warning)
 
                 // Positive button
-                .setPositiveButton("Set Default Preferences", new DialogInterface.OnClickListener() {
+                .setPositiveButton(ok, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
 //                        Intent myIntent = menu_new_view Intent(Intent.ACTION_VIEW, Uri.parse(moma_url));
 //                        startActivity(myIntent);
 
                         SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
                         sharedPrefs.edit().remove("multi_select_list_key").commit();
-                        Toast.makeText(getActivity(),"Preferences set to default!", Toast.LENGTH_LONG).show();
+                        Toast.makeText(getActivity(), toastMessage, Toast.LENGTH_LONG).show();
 
                         getFragmentManager().beginTransaction()
                                 .replace(android.R.id.content, new SettingsFragment())
@@ -47,7 +49,7 @@ public class AlertDialogFragment extends DialogFragment {
                 })
 
                 // Negative Button
-                .setNegativeButton("Not Now", new DialogInterface.OnClickListener() {
+                .setNegativeButton(cancel, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
                         // Do Nothing
                     }
