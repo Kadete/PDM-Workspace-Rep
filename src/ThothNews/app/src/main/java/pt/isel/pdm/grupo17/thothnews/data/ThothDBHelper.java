bc6338cc9c34e0c212650a1025fe99edf9b47ff2
Tiago Martins <tiagofcmartins@gmail.com>
2014-11-22 17:28:14 +0000

    Rename da nossa implementação de SQLiteOpenHelper

---
+++ src/ThothNews/app/src/main/java/pt/isel/pdm/grupo17/thothnews/data/ThothDBHelper.java
@@ -0,0 +1,35 @@
+package pt.isel.pdm.grupo17.thothnews.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class ThothDBHelper extends SQLiteOpenHelper {
+
+    /** Filename for SQLite file. */
+    final private static String DB_NAME = "thoth.db";
+    /** Schema version. */
+    public static final int DB_VERSION = 1;
+
+//    final private Context mContext;
+
+    public ThothDBHelper(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+//        this.mContext = context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(ThothContract.Clazz.CREATE_QUERY);
+        db.execSQL(ThothContract.News.CREATE_QUERY);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i2) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+//    void deleteDatabase() {
+//        mContext.deleteDatabase(DB_NAME);
+//    }
+}
