f7363d7394c24ae571f0c72657f5bd8380a15024
Tiago Martins <tiagofcmartins@gmail.com>
2014-11-25 14:05:52 +0000

    Adiciona implementação do serviço de update da base dados, comunicando directamente com a API do Thoth

--- src/ThothNews/app/src/main/java/pt/isel/pdm/grupo17/thothnews/services/ThothUpdateService.java
+++ src/ThothNews/app/src/main/java/pt/isel/pdm/grupo17/thothnews/services/ThothUpdateService.java
@@ -1,24 +1,78 @@
 package pt.isel.pdm.grupo17.thothnews.services;
 
 import android.app.IntentService;
-import android.content.Intent;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.text.Html;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
+
+import pt.isel.pdm.grupo17.thothnews.R;
+import pt.isel.pdm.grupo17.thothnews.activities.NewsActivity;
+import pt.isel.pdm.grupo17.thothnews.data.ThothContract;
+import pt.isel.pdm.grupo17.thothnews.utils.DateUtils;
+import pt.isel.pdm.grupo17.thothnews.utils.ParseUtils;
+
+import static pt.isel.pdm.grupo17.thothnews.utils.ParseUtils.TAG_ACTIVITY;
+import static pt.isel.pdm.grupo17.thothnews.utils.ParseUtils.d;
+import static pt.isel.pdm.grupo17.thothnews.utils.ParseUtils.readAllFrom;
 
 /**
  * An {@link IntentService} subclass for handling asynchronous task requests in
  * a service on a separate handler thread.
  * <p/>
- * TODO: Customize class - update intent actions, extra parameters and static
- * helper methods.
  */
 public class ThothUpdateService extends IntentService {
 
+    private static final String SERVICE_TAG = "ThothUpdateService";
     // IntentService can perform, e.g. ACTION_FETCH_NEW_ITEMS
     private static final String ACTION_NEWS_UPDATE = "pt.isel.pdm.grupo17.thothnews.services.action.NEWS_UPDATE";
     private static final String ACTION_CLASS_NEWS_UPDATE = "pt.isel.pdm.grupo17.thothnews.services.action.CLASS_NEWS_UPDATE";
     private static final String ACTION_CLASSES_UPDATE = "pt.isel.pdm.grupo17.thothnews.services.action.CLASSES_UPDATE";
 
-    private static final String CLASS_ID = "pt.isel.pdm.grupo17.thothnews.services.extra.CLASS_ID";
+    private static final String ARG_CLASS_ID = "pt.isel.pdm.grupo17.thothnews.services.extra.ARG_CLASS_ID";
+    private static final long ARG_CLASS_ID_DEFAULT_VALUE = -1;
+
+    public static final String URI_API_ROOT = "http://thoth.cc.e.ipl.pt/api/v1";
+    public static final String URI_CLASSES_LIST = URI_API_ROOT + "/classes";
+    public static final String URI_CLASS_NEWS = URI_API_ROOT + "/classes/%d/newsitems";
+    public static final String URI_NEWS_INFO = URI_API_ROOT + "/newsitems/%d";
+
+    private static final int COLUMN_CLASS_ID = 0;
+    class JsonThothClass{
+        public static final String ID = "id";
+        public static final String FULLNAME = "fullName";
+        public static final String COURSE_NAME = "courseUnitShortName";
+        public static final String LECTIVE_SEMESTER = "lectiveSemesterShortName";
+        public static final String NAME = "className";
+        public static final String TEACHER = "mainTeacherShortName";
+    }
+    class JsonThothNews{
+        public static final String ID = "id";
+        public static final String TITLE = "title";
+        public static final String WHEN = "when";
+        public static final String CONTENT = "content";
+    }
+
 
     public ThothUpdateService() {
         super("ThothUpdateService");
@@ -33,7 +87,7 @@ public class ThothUpdateService extends IntentService {
             } else if (ACTION_CLASSES_UPDATE.equals(action)) {
                 handleClassesUpdate();
             } else if (ACTION_CLASS_NEWS_UPDATE.equals(action)) {
-                final String classID = intent.getStringExtra(CLASS_ID);
+                final long classID = intent.getLongExtra(ARG_CLASS_ID, ARG_CLASS_ID_DEFAULT_VALUE);
                 handleClassNewsUpdate(classID);
             }
         }
@@ -69,29 +123,135 @@ public class ThothUpdateService extends IntentService {
      *
      * @see IntentService
      */
-    public static void startActionClassNewsUpdate(Context context, String classID) {
+    public static void startActionClassNewsUpdate(Context context, long classID) {
         Intent intent = new Intent(context, ThothUpdateService.class);
         intent.setAction(ACTION_CLASSES_UPDATE);
-        intent.putExtra(CLASS_ID, classID);
+        intent.putExtra(ARG_CLASS_ID, classID);
         context.startService(intent);
     }
 
     /**
-     * Handle action NewsUpdate in the provided background thread with the provided
-     * parameters.
+     * Handle action NewsUpdate in the provided background.
      */
     private void handleNewsUpdate() {
-        // TODO: Handle action Foo
-        throw new UnsupportedOperationException("Not yet implemented");
+        ContentResolver resolver = getContentResolver();
+        Cursor c = resolver.query(ThothContract.Clazz.ENROLLED_URI,new String[]{ThothContract.Clazz._ID}
+                ,String.format("%s = true",ThothContract.Clazz.ENROLLED),null,null);
+
+        long classID;
+        while(c.moveToNext()){
+            classID = c.getLong(COLUMN_CLASS_ID);
+            handleClassNewsUpdate(classID);
+        }
     }
+    public static final String TAG_SELECT_CLASS_ID = "Class-Selected-ID";
+    public static final String TAG_SELECT_CLASS_NAME = "Class-Selected-Name";
+
     /**
      * Handle action ClassNewsUpdate in the provided background thread with the provided
      * parameters.
      * @param classID
      */
-    private void handleClassNewsUpdate(String classID) {
-        // TODO: Handle action Foo
-        throw new UnsupportedOperationException("Not yet implemented");
+    private void handleClassNewsUpdate(long classID) {
+        if( classID == ARG_CLASS_ID_DEFAULT_VALUE){
+            return;
+        }
+        int addedNews = 0;
+        try {
+            String urlPath = String.format(URI_CLASS_NEWS,classID);
+            URL url = new URL(urlPath);
+            HttpURLConnection c = (HttpURLConnection) url.openConnection();
+            try {
+                InputStream is = c.getInputStream();
+                String data = readAllFrom(is);
+                final JSONArray thothNews = ParseUtils.parseElement(data, "newsItems");
+                ContentResolver resolver = getContentResolver();
+                // TODO: Insert in batch instead of one by one
+                // TODO: Only make request for content of the news that don't exist in the database
+                Uri classNewsUri = Uri.parse(String.format("%s/%d/enrolled",ThothContract.Clazz.CONTENT_URI,classID));
+                Cursor classNewsIDsCursor = resolver.query(classNewsUri, new String[]{ThothContract.News._ID},null,null,null);
+                List<Long> classNewsIDs = getListFromCursor(classNewsIDsCursor);
+                long currNewsID;
+                for (int idx = 0; idx < thothNews.length(); ++idx) {
+                    try {
+                        JSONObject jnews = thothNews.getJSONObject(idx), jnewsDetails;
+                        currNewsID = jnews.getLong(JsonThothNews.ID);
+                        if(!classNewsIDs.contains(currNewsID)){
+
+                            jnewsDetails = getNewsDetails(currNewsID);
+                            ContentValues currValues = new ContentValues();
+                            currValues.put(ThothContract.News._ID,jnews.getInt(JsonThothNews.ID));
+                            currValues.put(ThothContract.News.TITLE,jnews.getString(JsonThothNews.TITLE));
+
+                            String when = DateUtils.SAVE_DATE_FORMAT.parse(jnews.getString(JsonThothNews.WHEN)).toString();
+
+                            currValues.put(ThothContract.News.WHEN_CREATED, when);
+                            String content = String.valueOf(Html.fromHtml(jnewsDetails.getString(JsonThothNews.CONTENT)));
+                            currValues.put(ThothContract.News.CONTENT,content);
+                            currValues.put(ThothContract.News.CLASS_ID,classID);
+                            resolver.insert(ThothContract.News.CONTENT_URI, currValues);
+                            ++addedNews;
+                        }
+                    } catch (ParseException e) {
+                        Log.e(SERVICE_TAG,"ERROR: handleClassNewsUpdate(...) while parsing News' date from JSON response");
+                    }
+                }
+                String classSelection = ThothContract.Clazz._ID + " = "+classID;
+                final Cursor classNameCursor = resolver.query(ThothContract.Clazz.CONTENT_URI, new String[]{ThothContract.Clazz.FULL_NAME}
+                        ,classSelection,null,null);
+                if(classNameCursor.moveToNext()){
+                    String className = classNameCursor.getString(0);
+                    if(addedNews >0){
+                        Notification.Builder builder = new Notification.Builder(getApplicationContext())
+                                .setContentTitle("You have news to read on "+className)
+                                .setContentText("Click to open the application")
+                                .setAutoCancel(true)
+                                .setSmallIcon(R.drawable.thoth_icon)
+                                .setOngoing(false); //false => can drop notification on notification area, true => only dissapier if exectuted action for notification
+
+                        Intent i = new Intent(getApplicationContext(), NewsActivity.class);
+                        i.putExtra(TAG_SELECT_CLASS_ID, String.valueOf(classID));
+                        i.putExtra(TAG_SELECT_CLASS_NAME, className);
+                        PendingIntent pintent = PendingIntent.getActivity(getApplicationContext(), 1, i, 0);
+                        builder.setContentIntent(pintent);
+
+                        NotificationManager notificationManager = (NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
+                        notificationManager.notify(0, builder.build());
+                    }
+                }
+            } catch (JSONException e) {
+                Log.e(SERVICE_TAG, "ERROR: handleClassNewsUpdate(..) while parsing JSON response");
+                Log.e(SERVICE_TAG, e.getMessage());
+            } finally {
+                c.disconnect();
+            }
+        } catch (MalformedURLException e) {
+            d(TAG_ACTIVITY, "An error occurred while trying to create URL to request news list given classID:" + classID + "\nMessage: " + e.getMessage());
+        } catch (IOException e) {
+            d(TAG_ACTIVITY, "An error ocurred while trying to estabilish connection to Thoth API!\nMessage: " + e.getMessage());
+        }
+    }
+
+    /**
+     * Receives a cursor with only one column of type long and returns a List of the column values
+     * @param c Cursor with only one column composed of IDs of type long
+     * @return
+     */
+    private static List<Long> getListFromCursor(Cursor c){
+        List<Long> ids = new ArrayList<Long>();
+        while (c.moveToNext()){
+            ids.add(c.getLong(0));
+        }
+        return ids;
+    }
+
+    private static JSONObject getNewsDetails(long newsID) throws IOException, JSONException {
+        URL newsURL = new URL(String.format(URI_NEWS_INFO,newsID));
+        HttpURLConnection newsConn = (HttpURLConnection) newsURL.openConnection();
+        InputStream newsStream = newsConn.getInputStream();
+        String newsData = readAllFrom(newsStream);
+        return new JSONObject(newsData);
+
     }
 
     /**
@@ -99,7 +259,36 @@ public class ThothUpdateService extends IntentService {
      * parameters.
      */
     private void handleClassesUpdate() {
-        // TODO: Handle action Baz
-        throw new UnsupportedOperationException("Not yet implemented");
+        try{
+            URL url = new URL(URI_CLASSES_LIST);
+            HttpURLConnection c = (HttpURLConnection) url.openConnection();
+            try {
+                InputStream is = c.getInputStream();
+                String data = readAllFrom(is);
+                final JSONArray thothClasses = ParseUtils.parseClasses(data);
+                ContentResolver resolver = getContentResolver();
+                // TODO: Insert in batch instead of one by one
+                for(int idx = 0; idx < thothClasses.length();++idx){
+                    JSONObject jclass = thothClasses.getJSONObject(idx);
+                    ContentValues currValues = new ContentValues();
+                    currValues.put(ThothContract.Clazz._ID,jclass.getInt(JsonThothClass.ID));
+                    currValues.put(ThothContract.Clazz.FULL_NAME,jclass.getString(JsonThothClass.FULLNAME));
+                    currValues.put(ThothContract.Clazz.COURSE,jclass.getString(JsonThothClass.COURSE_NAME));
+                    currValues.put(ThothContract.Clazz.SEMESTER,jclass.getString(JsonThothClass.LECTIVE_SEMESTER));
+                    currValues.put(ThothContract.Clazz.SHORT_NAME,jclass.getString(JsonThothClass.NAME));
+                    currValues.put(ThothContract.Clazz.TEACHER,jclass.getString(JsonThothClass.TEACHER));
+                    resolver.insert(ThothContract.Clazz.CONTENT_URI,currValues);
+                }
+            } catch (JSONException e) {
+                Log.e(SERVICE_TAG, "ERROR: handleClassesUpdate(..) while parsing JSON response");
+                Log.e(SERVICE_TAG, e.getMessage());
+            } finally {
+                c.disconnect();
+            }
+        }catch (MalformedURLException e) {
+            d(TAG_ACTIVITY,"An error ocurred while trying to create URL to request classes list!\nMessage: "+e.getMessage());
+        } catch (IOException e) {
+            d(TAG_ACTIVITY,"An error ocurred while trying to estabilish connection to Thoth API!\nMessage: "+e.getMessage());
+        }
     }
 }
